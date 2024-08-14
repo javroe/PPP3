e18d17fd0e56f03d8bb07370404cc037ad2c4a87
javroe <bjayroe02@gmail.com>
2024-08-14 18:44:37 +0300

    first

---
+++ x1.2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+//find the bathroom from dorm number 840
+int main(){
+    std::cout << "Starting from dorm number 840:\n"
+    "Open the door that belongs to 840\n"
+    "Facing the door, move forward 3ft\n"
+    "Turn left\n"
+    "Move forward 50ft\n"
+    "Turn right\n"
+    "Open bathroom door\n"
+    "Move forward 3ft\n"
+    "End of sequence\n";
+    return 0;
+}
